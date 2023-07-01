3f5692d384c2b5c4898dea4faabe755121b54441
Jose Pose <jose.pose@eydevelopment.com>
2023-07-01 18:53:36 +0200

    feat: Create BaseApp structure
    Create BaseApp Structure to identify when launch test or app and limit app

--- cv/BaseApp.swift
+++ cv/BaseApp.swift
@@ -1,10 +1,33 @@
 import SwiftUI
 
 @main
-struct BaseApp: App {
+struct BaseApp {
+    
+    static func main() throws {
+        if NSClassFromString("XCTestCase") == nil {
+            LaunchApp.main()
+        } else {
+            // if tests test are running don't launch app
+            TestApp.main()
+        }
+    }
+    
+}
+
+struct LaunchApp: App {
+    
     var body: some Scene {
         WindowGroup {
             CharacterListConfigurator.createModule()
         }
     }
+    
+}
+
+struct TestApp: App {
+
+    var body: some Scene {
+        WindowGroup { Text("Running Unit Tests") }
+    }
+    
 }
